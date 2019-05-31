543d16ef2f411b098cd3e8504e4f0bc523b9ee35
Xemnas0 <franuzzo966@gmail.com>
2019-05-31 10:35:39 +0200

    gradient check

--- Model.py
+++ Model.py
@@ -18,12 +18,17 @@ class Model:
         # Gradient Descent hyperparameters
         self.lambda_L2 = None
         self.batch_size = None
+        self.n_cycles = None
         self.n_epochs = None
         self.eta_min = None
         self.eta_max = None
         self.eta = None
 
+        self.verbose = True
         self.scheduled_eta = None
+
+        # Flag for checking the gradient numerically
+        self.check_gradient = False
 
         # Model parameters
         self.W1 = None
@@ -32,7 +37,8 @@ class Model:
         self.b2 = None
 
         # History of training
-        self.history = {'train_loss': [], 'train_acc': [], 'val_loss': [], 'val_acc': []}
+        self.history = {'train_cost': [], 'train_loss': [], 'train_acc': [],
+                        'val_cost': [], 'val_loss': [], 'val_acc': [], 'eta': []}
 
     def compile(self, d, K, m=50):
         self.d = d
@@ -46,16 +52,17 @@ class Model:
     def _saveGDparams(self, GDparams):
         self.lambda_L2 = GDparams['lambda_L2']
         self.batch_size = GDparams['batch_size']
-        self.n_epochs = GDparams['n_epochs']
+        self.n_cycles = GDparams['n_cycles']
         self.n_s = GDparams['n_s']
         self.eta_min = GDparams['eta_min']
         self.eta_max = GDparams['eta_max']
+        self.verbose = GDparams['verbose']
 
         # Prepare schedule of the learning rate
         t = np.arange(self.n_s * 2)
         freq = 1 / (2 * self.n_s)
         self.scheduled_eta = (signal.sawtooth(2 * np.pi * t * freq, 0.5) + 1) / 2 * (
-                    self.eta_max - self.eta_min) + self.eta_min
+                self.eta_max - self.eta_min) + self.eta_min
 
         # Debug
         # plt.plot(self.scheduled_eta)
@@ -76,7 +83,7 @@ class Model:
         loss = -np.log(P[y, np.arange(N)]).mean()
         reg_term = self.lambda_L2 * (np.square(self.W1).sum() + np.square(self.W2).sum())
 
-        return loss + reg_term
+        return loss + reg_term, loss
 
     def compute_accuracy(self, X, y):
         P = self.forward_pass(X)[1]
@@ -88,9 +95,9 @@ class Model:
         """
         Computes cost and accuracy of the given data.
         """
-        cost = self.compute_cost(X, y)
+        cost, loss = self.compute_cost(X, y)
         accuracy = self.compute_accuracy(X, y)
-        return cost, accuracy
+        return cost, loss, accuracy
 
     def backward_pass(self, X, Y, H, P):
         ones_nb = np.ones(self.batch_size)
@@ -113,6 +120,9 @@ class Model:
 
         N = train_data[0].shape[1]
 
+        t_tot = self.n_cycles * 2 * self.n_s
+        self.n_epochs = int(float(t_tot) * self.batch_size / N)
+
         self._run_epochs(train_data, val_data, N)
 
         return self.history
@@ -120,35 +130,48 @@ class Model:
     def _run_epochs(self, train_data, val_data, N):
         n_batches = N // self.batch_size
 
-        for_epoch = trange(self.n_epochs, leave=True, unit='epoch')
+        if self.verbose:
+            for_epoch = trange(self.n_epochs, leave=True, unit='epoch')
 
-        for epoch in for_epoch:
-            self.shuffleData(train_data)
+            for epoch in for_epoch:
+                self.shuffleData(train_data)
 
-            # Evaluate for saving in history
-            train_loss, train_acc, val_loss, val_acc = self._update_history(train_data, val_data)
+                # Evaluate for saving in history
+                train_cost, _, train_acc, val_cost, _, val_acc = self._update_history(train_data, val_data)
 
-            for_epoch.set_description(f'train_loss: {train_loss:.4f}\ttrain_acc: {100*train_acc:.2f}%' + ' | ' +
-                                      f'val_loss: {val_loss:.4f}\ttrain_acc: {100*val_acc:.2f}% ')
+                for_epoch.set_description(f'train_cost: {train_cost:.4f}\ttrain_acc: {100*train_acc:.2f}%' + ' | ' +
+                                          f'val_cost: {val_cost:.4f}\tval_acc: {100*val_acc:.2f}% ')
 
-            self._run_batches(train_data, n_batches, epoch)
+                self._run_batches(train_data, n_batches, epoch)
+        else:
+
+            for epoch in range(self.n_epochs):
+                self.shuffleData(train_data)
+
+                # Evaluate for saving in history
+                train_cost, _, train_acc, val_cost, _, val_acc = self._update_history(train_data, val_data)
+
+                self._run_batches(train_data, n_batches, epoch)
 
     def _run_batches(self, train_data, n_batches, epoch):
 
         for b in range(n_batches):
-            X_batch, Y_batch = self._get_mini_batch(train_data, b)
+            X_batch, Y_batch, y_batch = self._get_mini_batch(train_data, b)
 
             self._update_eta(b, n_batches, epoch)
+            self.history['eta'].append(self.eta)
+            self._update_weights(X_batch, Y_batch, y_batch)
 
-            self._update_weights(X_batch, Y_batch)
-
-    def _update_weights(self, X_batch, Y_batch):
+    def _update_weights(self, X_batch, Y_batch, y_batch):
         H, P = self.forward_pass(X_batch)
         dL_dW1, dL_db1, dL_dW2, dL_db2 = self.backward_pass(X_batch, Y_batch, H, P)
 
-        self.W1 -= self.eta * dL_dW1 + 2 * self.lambda_L2 * self.W1
+        if self.check_gradient:
+            self._check_gradient_numerically(X_batch, Y_batch, y_batch, N=2, M=20)
+
+        self.W1 -= self.eta * (dL_dW1 + 2 * self.lambda_L2 * self.W1)
         self.b1 -= self.eta * dL_db1
-        self.W2 -= self.eta * dL_dW2 + 2 * self.lambda_L2 * self.W2
+        self.W2 -= self.eta * (dL_dW2 + 2 * self.lambda_L2 * self.W2)
         self.b2 -= self.eta * dL_db2
 
     def _get_mini_batch(self, data, j):
@@ -156,7 +179,8 @@ class Model:
         j_end = (j + 1) * self.batch_size
         X_batch = data[0][:, j_start:j_end]
         Y_batch = data[1][:, j_start:j_end]
-        return X_batch, Y_batch
+        y_batch = data[2][j_start:j_end]
+        return X_batch, Y_batch, y_batch
 
     def shuffleData(self, data):
         """
@@ -170,15 +194,17 @@ class Model:
         data[2][:] = data[2][indeces]
 
     def _update_history(self, train_data, val_data):
-        train_loss, train_acc = self.evaluate(train_data[0], train_data[2])
-        val_loss, val_acc = self.evaluate(val_data[0], val_data[2])
+        train_cost, train_loss, train_acc = self.evaluate(train_data[0], train_data[2])
+        val_cost, val_loss, val_acc = self.evaluate(val_data[0], val_data[2])
 
+        self.history['train_cost'].append(train_cost)
         self.history['train_loss'].append(train_loss)
         self.history['train_acc'].append(train_acc)
+        self.history['val_cost'].append(val_cost)
         self.history['val_loss'].append(val_loss)
         self.history['val_acc'].append(val_acc)
 
-        return train_loss, train_acc, val_loss, val_acc
+        return train_cost, train_loss, train_acc, val_cost, val_loss, val_acc
 
     def _update_eta(self, b, batches, epoch):
 
@@ -203,3 +229,115 @@ class Model:
         val_data.append(train_data[2][end_train:])
 
         return train_data_new, val_data
+
+    """
+    Functions for numerical check of the gradient
+    """
+
+    def forward_pass_num(self, X, W1, W2):
+        S1 = W1.dot(X) + self.b1
+        H = np.maximum(0, S1)
+        S = W2.dot(H) + self.b2
+        P = softmax(S)
+        return H, P
+
+    def compute_cost_num(self, X, y, W1, W2, P=None):
+        N = X.shape[1]
+        if P is None:
+            P = self.forward_pass_num(X, W1, W2)[1]
+
+        loss = -np.log(P[y, np.arange(N)]).mean()
+        reg_term = self.lambda_L2 * (np.square(W1).sum() + np.square(W2).sum())
+
+        return loss + reg_term, loss
+
+    def backward_pass_num(self, X, Y, W1, W2, H, P):
+        ones_nb = np.ones(X.shape[1])
+        c = 1 / X.shape[1]
+        G = P - Y
+        dL_dW2 = c * G.dot(H.T)
+        dL_db2 = c * G.dot(ones_nb).reshape(self.K, 1)
+        G = W2.T.dot(G)
+        G = G * (H > 0)
+        dL_dW1 = c * G.dot(X.T)
+        dL_db1 = c * G.dot(ones_nb).reshape(W1.shape[0], 1)
+
+        return dL_dW1, dL_db1, dL_dW2, dL_db2
+
+    def _compute_grad_num_slow(self, X, y, W1, W2, h=1e-5):
+
+        dL_dW1 = np.zeros(W1.shape)
+        dL_db1 = np.zeros(self.b1.shape)
+        dL_dW2 = np.zeros(W2.shape)
+        dL_db2 = np.zeros(self.b2.shape)
+
+        for i in range(len(self.b1)):
+            self.b1[i] -= h
+            c1 = self.compute_cost_num(X, y, W1, W2)[0]
+            self.b1[i] += 2 * h
+            c2 = self.compute_cost_num(X, y, W1, W2)[0]
+            self.b1[i] -= h
+            dL_db1[i] = (c2 - c1) / (2 * h)
+
+        for i in range(len(self.b2)):
+            self.b2[i] -= h
+            c1 = self.compute_cost_num(X, y, W1, W2)[0]
+            self.b2[i] += 2 * h
+            c2 = self.compute_cost_num(X, y, W1, W2)[0]
+            self.b2[i] -= h
+            dL_db2[i] = (c2 - c1) / (2 * h)
+
+        for i in range(W1.shape[0]):
+            for j in range(W1.shape[1]):
+                W1[i, j] -= h
+                c1 = self.compute_cost_num(X, y, W1, W2)[0]
+                W1[i, j] += 2 * h
+                c2 = self.compute_cost_num(X, y, W1, W2)[0]
+                W1[i, j] -= h
+                dL_dW1[i, j] = (c2 - c1) / (2 * h)
+
+        for i in range(W2.shape[0]):
+            for j in range(W2.shape[1]):
+                W2[i, j] -= h
+                c1 = self.compute_cost_num(X, y, W1, W2)[0]
+                W2[i, j] += 2 * h
+                c2 = self.compute_cost_num(X, y, W1, W2)[0]
+                W2[i, j] -= h
+                dL_dW2[i, j] = (c2 - c1) / (2 * h)
+
+        return dL_dW1, dL_db1, dL_dW2, dL_db2
+
+    def _check_gradient_numerically(self, X_batch, Y_batch, y_batch, N, M):
+        """
+        :param N: Number of samples to test
+        :param M: Number of featuers to test
+        """
+
+        H, P = self.forward_pass_num(X_batch[:M, :N], W1=self.W1[:, :M], W2=self.W2)
+        dL_dW1_check, dL_db1_check, dL_dW2_check, dL_db2_check = self.backward_pass_num(X_batch[:M, :N],
+                                                                                        Y_batch[:, :N],
+                                                                                        self.W1[:, :M], self.W2, H,
+                                                                                        P)
+        dL_dW1_n, dL_db1_n, dL_dW2_n, dL_db2_n = self._compute_grad_num_slow(X_batch[:M, :N], y_batch[:N],
+                                                                             self.W1[:, :M], self.W2)
+
+        ok_W1 = self._compute_numerical_error(dL_dW1_n, dL_dW1_check)
+        ok_b1 = self._compute_numerical_error(dL_db1_n, dL_db1_check)
+        ok_W2 = self._compute_numerical_error(dL_dW2_n, dL_dW2_check)
+        ok_b2 = self._compute_numerical_error(dL_db2_n, dL_db2_check)
+        print(f'Sanity check: {ok_W1}, {ok_b1}, {ok_W2}, {ok_b2}')
+
+    def _compute_numerical_error(self, A_num, A_check):
+        eps = 1e-8
+        tolerance_error = 1e-5
+        num = np.abs(A_check - A_num)
+        den = np.maximum(eps, np.abs(A_num) + np.abs(A_check))
+        err = num / den
+        max_err = err.max()
+        n_ok = (err < tolerance_error).sum()
+        p_ok = n_ok / A_num.size * 100
+
+        print(f'Max error: {max_err}\nPercentage of values under max tolerated value: {p_ok}\n' +
+              f'eps: {eps}\tMax tolerated error: {tolerance_error}')
+
+        return p_ok
--- main.py
+++ main.py
@@ -4,46 +4,73 @@ from utilities import readDataset, normalize
 from Model import Model
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pickle
+from tqdm import tqdm
 
 sns.set()
 
+task = {
+    'partial_data': True,
+    'lambda_search': False
+}
+
+# Gradient Descent parameters
+GDparams = {'batch_size': 100,
+            'eta_min': 1e-5,
+            'eta_max': 1e-1,
+            'n_s': 900,
+            'n_cycles': 2,
+            'lambda_L2': 0.01,
+            'verbose': False}
+# Lambda search
+n_lambda_search = 100
+
+val_split = 0.1
+
+
 def plot_history(history, GDparams):
-    fig = plt.figure(1, figsize=(12, 5))
-    gs1 = gridspec.GridSpec(1, 2)
+    fig = plt.figure(1, figsize=(14, 4))
+    gs1 = gridspec.GridSpec(1, 3)
     ax = [fig.add_subplot(subplot) for subplot in gs1]
 
-    ax[0].set_title('Cost')
-    ax[0].plot(history['train_loss'], sns.xkcd_rgb["pale red"], label='Train')
-    ax[0].plot(history['val_loss'], sns.xkcd_rgb["denim blue"], label='Val')
-    ax[0].axhline(history['test_loss'], color=sns.xkcd_rgb["medium green"], label='Test')
-    ax[0].set_xlabel('Epochs')
-    ax[0].legend()
-
-    ax[1].set_title('Accuracy')
-    ax[1].plot(history['train_acc'], sns.xkcd_rgb["pale red"], label='Train')
-    ax[1].plot(history['val_acc'], sns.xkcd_rgb["denim blue"], label='Val')
-    ax[1].axhline(history['test_acc'], color=sns.xkcd_rgb["medium green"], label='Test')
-    ax[1].set_xlabel('Epochs')
-    ax[1].legend()
+    for i, (title, metric) in enumerate(zip(['Cost', 'Loss', 'Accuracy'], ['cost', 'loss', 'acc'])):
+        ax[i].set_title(title)
+        ax[i].plot(history[f'train_{metric}'], sns.xkcd_rgb["pale red"], label='Train')
+        ax[i].plot(history[f'val_{metric}'], sns.xkcd_rgb["denim blue"], label='Val')
+        ax[i].axhline(history[f'test_{metric}'], color=sns.xkcd_rgb["medium green"], label='Test')
+        ax[i].text(0.5, history[f'test_{metric}'],
+                   '{0:.4f}'.format(history[f'test_{metric}']), fontsize=12, va='center', ha='center',
+                   backgroundcolor='w')
+        ax[i].set_ylim(bottom=0)
+        ax[i].set_xlabel('Epochs')
+        ax[i].legend()
 
     main_title = 'batch_size={0}, $\eta_{{min}}={1}$, $\eta_{{max}}={2}$, $n_s={3}$'.format(
         GDparams['batch_size'], GDparams['eta_min'], GDparams['eta_max'], GDparams['n_s']
-    ) + ', $\lambda={0}$, n_epochs={1}'.format(GDparams['lambda_L2'], GDparams['n_epochs'])
+    ) + ', $\lambda={0}$, n_cycles={1}'.format(GDparams['lambda_L2'], GDparams['n_cycles'])
     fig.suptitle(main_title, fontsize=18)
 
     gs1.tight_layout(fig, rect=[0, 0.03, 1, 0.95])
 
     fig.show()
 
+    fig.savefig('second_experiment.png'.format(GDparams['lambda_L2']))
 
-if __name__ == '__main__':
+    plt.plot(history['eta'])
+    plt.show()
+
+
+"""
+Section for single experiment
+"""
+if __name__ == '__main__' and task['partial_data']:
 
     # Loading datasets
-    load_full_data = True
+    load_full_data = False
     if load_full_data:
         X_train, Y_train, y_train = readDataset('Datasets/cifar-10-batches-py/data_batch_1')
-        for i in range(2,6):
-            X, Y ,y = readDataset(f'Datasets/cifar-10-batches-py/data_batch_{i}')
+        for i in range(2, 6):
+            X, Y, y = readDataset(f'Datasets/cifar-10-batches-py/data_batch_{i}')
             X_train = np.hstack((X_train, X))
             Y_train = np.hstack((Y_train, Y))
             y_train = np.hstack((y_train, y))
@@ -58,14 +85,6 @@ if __name__ == '__main__':
     d, N = X_train.shape
     K = y_train.max() + 1
 
-    # Gradient Descent parameters
-    GDparams = {'batch_size': 100,
-                'eta_min': 1e-5,
-                'eta_max': 1e-1,
-                'n_s': 800,
-                'n_epochs': 10,
-                'lambda_L2': 0.0001}
-
     # Normalization of the dataset
     X_train, mean, std = normalize(X_train)
     if not load_full_data:
@@ -76,10 +95,72 @@ if __name__ == '__main__':
     model.compile(d, K, m=50)
 
     if load_full_data:
-        history = model.fit((X_train, Y_train, y_train), GDparams, val_split=0.1)
+        history = model.fit((X_train, Y_train, y_train), GDparams, val_split=val_split)
     else:
         history = model.fit((X_train, Y_train, y_train), GDparams, val_data=(X_val, Y_val, y_val))
 
-    history['test_loss'], history['test_acc'] = model.evaluate(X_test, y_test)
-    print('Test loss: {0:.4f}\tTest acc: {1:.2f}%'.format(history['test_loss'], history['test_acc'] * 100))
+    history['test_cost'], history['test_loss'], history['test_acc'] = model.evaluate(X_test, y_test)
+    print('Test cost: {0:.4f}\tTest acc: {1:.2f}%'.format(history['test_cost'], history['test_acc'] * 100))
     plot_history(history, GDparams)
+
+"""
+Section for search of lambda
+"""
+if __name__ == '__main__' and task['lambda_search']:
+
+    # Loading datasets
+    load_full_data = True
+    if load_full_data:
+        X_train, Y_train, y_train = readDataset('Datasets/cifar-10-batches-py/data_batch_1')
+        for i in range(2, 6):
+            X, Y, y = readDataset(f'Datasets/cifar-10-batches-py/data_batch_{i}')
+            X_train = np.hstack((X_train, X))
+            Y_train = np.hstack((Y_train, Y))
+            y_train = np.hstack((y_train, y))
+        X_test, Y_test, y_test = readDataset('Datasets/cifar-10-batches-py/test_batch')
+
+    else:
+        X_train, Y_train, y_train = readDataset('Datasets/cifar-10-batches-py/data_batch_1')
+        X_val, Y_val, y_val = readDataset('Datasets/cifar-10-batches-py/data_batch_2')
+        X_test, Y_test, y_test = readDataset('Datasets/cifar-10-batches-py/test_batch')
+
+    # Data characteristics
+    d, N = X_train.shape
+    K = y_train.max() + 1
+
+    # Normalization of the dataset
+    X_train, mean, std = normalize(X_train)
+    if not load_full_data:
+        X_val = (X_val - mean) / std
+    X_test = (X_test - mean) / std
+
+    l_min = -5
+    l_max = -1
+    stats_lambdas = {'lambda': [], 'val_acc': []}
+    for i in tqdm(range(n_lambda_search)):
+        l = l_min + (l_max - l_min) * np.random.rand()
+        GDparams['lambda_L2'] = 10 ** l
+        # print(GDparams['lambda_L2'])
+        model = Model()
+        model.compile(d, K, m=50)
+
+        if load_full_data:
+            history = model.fit((X_train, Y_train, y_train), GDparams, val_split=val_split)
+        else:
+            history = model.fit((X_train, Y_train, y_train), GDparams, val_data=(X_val, Y_val, y_val))
+
+        stats_lambdas['lambda'].append(GDparams['lambda_L2'])
+        stats_lambdas['val_acc'].append(history['val_acc'][-1])
+
+        # history['test_cost'], history['test_loss'], history['test_acc'] = model.evaluate(X_test, y_test)
+        # print('Test loss: {0:.4f}\tTest acc: {1:.2f}%'.format(history['test_loss'], history['test_acc'] * 100))
+        # plot_history(history, GDparams)
+
+    pickle_out = open("stats_lambdas.pickle", "wb")
+    pickle.dump(stats_lambdas, pickle_out)
+    pickle_out.close()
+
+    fig, ax = plt.subplots()
+    ax.plot(stats_lambdas['lambda'], stats_lambdas['val_acc'])
+    fig.show()
+    fig.savefig('lambda_search_{0}_{1}.png'.format(l_min, l_max))
